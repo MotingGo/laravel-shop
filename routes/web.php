f2131fc989524e16789cd9a7d97bcc1d99fca21a
moting <motingphp@gmail.com>
2019-11-03 15:30:12 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PagesController@root')->name('boot');
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
