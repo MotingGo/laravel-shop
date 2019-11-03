7aaf0f2747af4c104b3f9e34fa3c15e2307edf0e
moting <motingphp@gmail.com>
2019-11-03 02:01:10 +0000

    邮箱验证功能

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'PagesController@root')->name('boot');
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
