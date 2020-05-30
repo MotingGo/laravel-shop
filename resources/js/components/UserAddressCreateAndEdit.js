e6fae5a2fdc4369fc65be252d8103a48b6638f7f
Moting <motingphp@gmail.com>
2020-05-31 00:35:22 +0800

    新增收货地址-提交页面样式

---
+++ resources/js/components/UserAddressCreateAndEdit.js
@@ -0,0 +1,21 @@
+// 注册一个名为 user-addresses-create-and-edit 的组件
+Vue.component('user-address-create-and-edit', {
+  // 组件的数据
+  date() {
+    return {
+      province: '', // 省
+      city: '', // 市
+      district: '', // 区
+    }
+  },
+  methods: {
+    // 把参数 val 中的值保存到组件的数据中
+    onDistrictChaned(val) {
+      if (val.length === 3) {
+        this.province = val[0];
+        this.city = val[1];
+	this.district = val[2];
+      }
+    }
+  }
+});
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PagesController@root')->name('boot');
+Route::get('/', 'PagesController@root')->name('boot')->middleware('verified');
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
